59404addc5e33772a1c8599c2cbafa9305899c35
Caio Rodrigo <contato@caiorodrigocev.com.br>
2025-09-07 00:17:10 -0300

    Adicionado insert empresa

--- init/30-seed.sql
+++ init/30-seed.sql
@@ -1,3 +1,6 @@
 \connect "DBSYSTEM";
 INSERT INTO public.usu (usunome, usuemail, ususenha, usuadm)
 VALUES ('Administrador', 'admin@admin.com', md5('admin@'), 'S');
+
+INSERT INTO public.emp (emprazao, empwhatsapp1, empwhatsapp2)
+VALUES('Empresa', '', '');
